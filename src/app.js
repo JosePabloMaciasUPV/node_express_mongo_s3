0ddbc27ea100a6b179861993bcf246027b082537
JosePabloMaciasUPV <reydepicas@localhost.localdomain>
2021-07-11 16:52:53 +0000

    Second version

--- src/app.js
+++ src/app.js
@@ -4,18 +4,15 @@ import FileRouter from './routes/files.routes';
 import AuthRouter from './routes/auth.routes';
 const app =express();
 const cors = require('cors');
+//About spa file hosting provider
+const path = require('path');
 app.use(cors({
     origin: '*'
 }));
 app.use(morgan('dev'));
-
-//About spa file hosting provider
-const path = require('path')
-app.use('/', express.static(path.join(__dirname, '../public')))
-app.get('/*', async (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
 app.use(express.json());
 app.use(FileRouter);
 app.use(AuthRouter);
+app.use('/', express.static(path.join(__dirname, '../public')))
+
 export default app;
